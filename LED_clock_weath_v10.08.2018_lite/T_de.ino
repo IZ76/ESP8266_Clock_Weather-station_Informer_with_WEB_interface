3d4ddde66a58ea49964bec3cc32e154d3ee56d9d
Ivan Zakharov <37518731+IZ76@users.noreply.github.com>
2018-08-14 13:57:44 +0300

    Add files via upload

---
+++ LED_clock_weath_v10.08.2018_lite/T_de.ino
@@ -0,0 +1,96 @@
+void deText(){
+  tMes = "Schatzi!!! Das ist für dich!!!";
+  tNow = "jetzt";
+  tPress = "mmHg";
+  tSpeed = "m/s";
+  tMin = "min.";
+  tTom = "Morgen";
+  tKurs = "Kurs Privat24";
+  tSale = "kaufen/verkaufen.";
+  tYour = "Ihr";
+  tPoint = "Stellen Sie die Verbindung mit WiFi-Clock her, und geben Sie im Browser die Adresse: 192.168.4.1         ";
+
+  tWarsaw = "Warsaw";
+  tKrivoy = "Krivoy Rog";
+  tKyiv = "Kyiv";
+  tOdessa = "Odessa";
+  tDnepr = "Dnepr";
+  tCherkassy = "Cherkassy";
+  tLviv = "Lviv";
+  tKherson = "Kherson";
+  tNikolaev = "Nikolaev";
+  tZaporozhye = "Zaporozhye";
+  tKharkiv = "Kharkiv";
+  tDonetsk = "Donetsk";
+  tLugansk = "Lugansk";
+  tPoltava = "Poltav";
+  tSumy = "Sumy";
+  tChernigov = "Chernigov";
+  tZhytomyr = "Zhytomyr";
+  tRivne = "Rivne";
+  tKropivnitsky = "Kropivnitsky";
+  tUman = "Uman";
+  tVinnitsa = "Vinnitsa";
+  tKhmelnitsky = "Khmelnitsky";
+  tTernopil = "Ternopil";
+  tLutsk = "Lutsk";
+  tKovel = "Kovel";
+  tChernivtsi = "Chernivtsi";
+  tIvano = "Ivano-Frankovsk";
+  tStry = "Stryj";
+  tMukachevo = "Mukachevo";
+  tUzhgorod = "Uzhgorod";
+  tCzenstochowa = "Czenstochowa";
+  tKrakow = "Krakow";
+  tLublinec = "Lublinec";
+
+  tJanuary = "Januar";
+  tFebruary = "Februar";
+  tMarch = "März";
+  tApril = "April";
+  tMay = "Mai";
+  tJune = "Juni";
+  tJuly = "Juli";
+  tAugust = "August";
+  tSeptember = "September";
+  tOctober = "Oktober";
+  tNovember = "November";
+  tDecember = "Dezember";
+  
+  tMonday = "Montag";
+  tTuesday = "Dienstag";
+  tWednesday = "Mittwoch";
+  tThursday = "Donnerstag";
+  tFriday = "Freitag";
+  tSaturday = "Samstag";
+  tSunday = "Sonntag";
+  
+  tClearSky = "wolkenlos";
+  tSkyIsClear = "wolkenlos";
+  tFewClouds = "teils bewölkt";
+  tScatteredClouds = "bewölkt";
+  tBrokenClouds = "bewölkt";
+  tOvercastClouds = "bedeckt";
+  tLightRain = "regenschauer";  
+  tModerateRain = "leichter  Regen";
+  tLightIntensityShowerRain = "leichter Regen";
+  tShowerRain="Regenguss";
+  tHeavyIntensityRain = "starkregen";
+  tVeryHeavyRain = "sehr starker Regen";
+  tThunderstorm = "gewitter";
+  tHaze = "nebel";
+  tFog = "nebel";
+  tMist = "hochnebel";
+  tShowerSleet = "schneeregen";
+  tLightSnow = "leichter Schneeschauer";
+  tLightShowerSnow = "leichter Schneefall"; 
+  tSnow = "schneefall";
+  tWeatrNot = "   Keine Wetteraktualisierungen seit 6 Stunden!!!   ";
+  tWeatrTN = "Keine Wetteraktualisierungen - ";
+  if(printCom) {
+    printTime();
+    Serial.println("Text DE.");
+  }
+}
+
+
